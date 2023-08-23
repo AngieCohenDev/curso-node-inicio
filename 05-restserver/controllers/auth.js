18135b358c988f343882b9b0c759b0dcc2e12e7c
Angie Cohen <angiedev2001@gmail.com>
2023-08-23 19:32:33 -0300

    se genero el jwt

--- 05-restserver/controllers/auth.js
+++ 05-restserver/controllers/auth.js
@@ -1,14 +1,49 @@
 const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require("bcryptjs");
+const { generarJWt } = require("../helpers/generar-jwt");
 
+const login = async (req, res = response) => {
+  const { mail, password } = req.body;
 
-const login = (req, res = response) => {
+  try {
+    // verificar si el email existe
+    const usuario = await Usuario.findOne({ mail });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
+    }
+    //si el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado:false",
+      });
+    }
+
+    //verificar password
+
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
+    }
+
+    //Generar el JWT
+    const token = await generarJWt(usuario.id);
 
     res.json({
-        msg:'Login ok'
-    })
-}
+      usuario,
+      token
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
-
-module.exports ={
-    login
-}
+module.exports = {
+  login,
+};
