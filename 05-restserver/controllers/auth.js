6cd75d1b40f8d470d13793a2da0b8b0293852fc1
Angie Cohen <angiedev2001@gmail.com>
2023-08-25 21:18:47 -0300

    intentado borrar usuario de google

--- 05-restserver/controllers/auth.js
+++ 05-restserver/controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcryptjs = require("bcryptjs");
 const { generarJWt } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+const { json } = require("body-parser");
 
 const login = async (req, res = response) => {
   const { mail, password } = req.body;
@@ -35,7 +37,7 @@ const login = async (req, res = response) => {
 
     res.json({
       usuario,
-      token
+      token,
     });
   } catch (error) {
     res.status(500).json({
@@ -44,17 +46,51 @@ const login = async (req, res = response) => {
   }
 };
 
-const googleSignIn = async(req, res = response) => {
+const googleSignIn = async (req, res = response) => {
 
   const {id_token} = req.body;
 
-  res.json({
-    msg:'Todo bien',
-    id_token
-  })
-} 
+  try {
+
+    const {nombre, img, mail} = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({mail});
+    if(!usuario){
+      const data = {
+        nombre,
+        mail,
+        password: "1234567",
+        img,
+        google:true,
+        rol: 'ADMIN_ROLE'
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    if(!usuario.estado){
+      return res.status(401).json({
+        msg:'Hable con el admin'
+      })
+    }
+
+    // Generar el JWT
+    const token = await generarJWt(usuario.id);
+
+    res.json({
+      usuario,
+      token
+    })
+  } catch (error) {
+      res.status(400).json({
+      ok: false,
+      msg:'El token no se pudo verificar'
+    })
+  }
+};
 
 module.exports = {
   login,
-  googleSignIn
+  googleSignIn,
 };
