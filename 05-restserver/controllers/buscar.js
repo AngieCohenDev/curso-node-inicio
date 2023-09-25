d995aa0671ca1812f8391721f2ac3a8d5f7d52e6
Angie Cohen <angiedev2001@gmail.com>
2023-09-25 15:59:11 -0300

    Creacion del sistema de busqueda

--- 05-restserver/controllers/buscar.js
+++ 05-restserver/controllers/buscar.js
@@ -1,15 +1,64 @@
-const { response } = require("express")
+const { response } = require("express");
+const {ObjectId} = require("mongoose").Types;
+const {Usuario, Categoria, Producto} = require("../models");
 
-const buscar = (req, res = response) =>{
+const coleccionesPermitidas = ["usuario", "categoria", "productos", "roles"];
 
-    const {coleccion, termino} = req.params;
+
+const buscarUsuarios = async(termino = '', res = response) => {
+    const esMongoID = ObjectId.isValid(termino);
+
+    if(esMongoID) {
+        const usuario = await Usuario.findById(termino);
+        return res.json({
+            results: (usuario) ? [usuario] : []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const usuarios = await Usuario.find({
+        $or: [{nombre:regex}, {mail:regex}],
+        $and: [{estado: true}]
+    });
 
     res.json({
-        coleccion,
-        termino
+        results: usuarios
     })
+
+
 }
 
+const buscar = (req, res = response) => {
+  const { coleccion, termino } = req.params;
+
+  if (!coleccionesPermitidas.includes(coleccion)) {
+    return res.status(400).json({
+      msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case "usuario":
+        buscarUsuarios(termino, res);
+
+    break;
+    case "categoria":
+
+    break;
+    case "productos":
+
+    break;
+
+    default:
+        res.status(500).json({
+            msg:`Se me olvido hacer esta busqueda`
+        })
+  }
+
+};
+
 module.exports = {
-    buscar
-}
+  buscar,
+  buscarUsuarios
+};
