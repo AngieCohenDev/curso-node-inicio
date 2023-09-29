0c4a900f7a268cce54315b3c78a6b8454b22d819
Angie Cohen <angiedev2001@gmail.com>
2023-09-29 16:14:46 -0300

    se establecio el codigo para subir archivos

--- 05-restserver/controllers/uploads.js
+++ 05-restserver/controllers/uploads.js
@@ -1,31 +1,42 @@
-const path = require('path');
-const {response} = require('express');
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const { response } = require("express");
 
-const cargarArchivo = ( req, res = response) =>{
+const cargarArchivo = (req, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).json({
+      msg: "No hay archivo para subir",
+    });
+    return;
+  }
 
-      
-        if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-          res.status(400).json({
-            msg: 'No hay archivo para subir'
-          });
-          return;
-        }
-      
-        const {archivo} = req.files;
-      
-        const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-      
-        archivo.mv(uploadPath,(err) => {
-          if (err) {
-            return res.status(500).json({err});
-          }
-      
-          res.json({msg: 'File uploaded to ' + uploadPath});
-        });
-      
-}
+  const { archivo } = req.files;
 
+  const nombreCortado = archivo.name.split(".");
 
-module.exports ={
-    cargarArchivo
-}
+  const extension = nombreCortado[nombreCortado.length - 1];
+
+  // Validar la extension
+  const extensionesValidas = ["png", "jpg", "gif", "jpeg"];
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extension ${extension} no es permitida, ${extensionesValidas}`,
+    });
+  }
+
+  const nombreTemp = uuidv4() + '.' + extension;
+
+  const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
+
+  archivo.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ err });
+    }
+
+    res.json({ msg: "File uploaded to " + uploadPath });
+  });
+};
+
+module.exports = {
+  cargarArchivo,
+};
