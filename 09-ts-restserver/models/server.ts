b6dc18f434081b956a21c933818b005e04faf414
Angie Cohen <angiedev2001@gmail.com>
2023-12-28 14:24:55 -0300

    configuracion de los middlewares y cors

--- 09-ts-restserver/models/server.ts
+++ 09-ts-restserver/models/server.ts
@@ -1,5 +1,6 @@
 import express, {Application} from 'express';
 import userRoutes from '../routes/usuarios'
+import cors from 'cors'
 
 
 class Server {
@@ -14,8 +15,22 @@ class Server {
         this.app  = express();
         this.port = process.env.PORT || '8000';
 
+        this.middlewares();
+
         // Definir mis rutas
         this.routes();
+
+    }
+
+    middlewares(){
+        // CORS
+        this.app.use(cors());
+
+        // Lectura del body
+        this.app.use(express.json());
+
+        // Carpeta publica
+        this.app.use(express.static('public'));
 
     }
 
